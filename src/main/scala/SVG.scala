738d0aac9d8b05b69d642ce49531b4c1a6259244
Brian Hong <brian@bhong.dev>
2021-04-12 03:52:04 -0400

    We're not in Canvas anymore
    Canvas can't draw Path objects directly
    Just group the path objects and draw the group itself as a node

--- src/main/scala/SVG.scala
+++ src/main/scala/SVG.scala
@@ -4,6 +4,9 @@ import java.io.{File, FileReader}
 
 import javax.xml.parsers.SAXParserFactory
 import javax.xml.stream.{XMLInputFactory, XMLStreamConstants}
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.{FillRule, Path, SVGPath}
+import scalafx.scene.transform.{Scale, Transform, Translate}
 
 import scala.xml._
 
@@ -26,4 +29,42 @@ object SVGParser {
 class SVG(val xml: Elem ) {
   val attributes = xml.attributes.map(md => (md.key -> md.value)).toMap
   override def toString = xml.toString
+
+//  val node: scalafx.scene.Node
+  val paths: List[SVGPath] = getPaths(xml, 0)
+
+  private def getPaths(e: Node, depth: Int): List[SVGPath] = {
+    e.child.map {
+      case g @ Elem(_,"g",attr,_,_*) => {
+        val transforms = "(\\S+)\\(([^)]*)\\)".r.findAllMatchIn(
+          (attr.get("transform") getOrElse "").toString
+        ).map( m => {
+          val args = m.group(2).split("[,\\s]").map(x => x.toDouble).toSeq
+          m.group(1) match {
+            case "translate" => new Translate(args(0), args(1))
+            case "scale" => new Scale(args(0), args(1))
+//            case _ => ()
+          }
+        }).toList
+        println(transforms)
+
+        println(s"Found <g> at ${depth}"); List()
+        val c = getPaths(g, depth+1)
+        for(p <- c; t <- transforms) p.transforms.add(t)
+        c.map(x => println(x))
+        c
+      }
+      case Elem(_,"path",attr,_,_*) => List(new SVGPath{
+        content = (attr.get("d") getOrElse "").toString
+//        this.setFill(Color.Transparent)
+//        this.setStroke(Color.Red)
+//        this.setStrokeWidth(2.0)
+      })
+      case u @ Elem(_, tag, _,_,_*) => {
+        println(s"Found tag ${tag} at ${depth}")
+        getPaths(u, depth+1)
+      }
+      case u => getPaths(u, depth+1)
+    }
+  }.toList.flatten
 }
--- src/main/scala/SVGExplorer.scala
+++ src/main/scala/SVGExplorer.scala
@@ -8,6 +8,7 @@ import scalafx.scene.control.{Menu, MenuBar, MenuItem}
 import scalafx.scene.input.{KeyCode, KeyCodeCombination, KeyCombination}
 import scalafx.scene.layout.{HBox, VBox}
 import scalafx.scene.paint.Color
+import scalafx.scene.shape.Path
 import scalafx.stage.{FileChooser, Stage, Window}
 import scalafx.stage.FileChooser.ExtensionFilter
 
@@ -27,7 +28,7 @@ object SVGExplorer extends JFXApp {
 class SVGExplorer(implicit w: Window) {
 
   private var svg = SVGParser.empty
-  val canvas = new Canvas(3000, 3000)
+  val canvas = new scalafx.scene.Group() //new Canvas(3000, 3000)
 
   val shortcuts = Map(
     new KeyCodeCombination(KeyCode.O, KeyCombination.ControlDown) -> (() => openAction),
@@ -60,115 +61,8 @@ class SVGExplorer(implicit w: Window) {
   }
 
   private def draw = {
-    val gc = canvas.graphicsContext2D
-    gc.clearRect(0, 0, canvas.getWidth, canvas.getHeight)
-
-    gc.setStroke(Color.Black)
-    gc.setFill(Color.Red)
-    gc.setLineWidth(1.0)
-
     val viewBox = (svg.attributes.get("viewBox") getOrElse "0 0 100 100").toString.split("[,\\s+]").map(x => x.toDouble)
-//    canvas.width = viewBox(2)
-//    canvas.height = viewBox(3)
-//    gc.fillRect(viewBox(0), viewBox(1), viewBox(2), viewBox(3))
-
-    val transformRegex = "(\\S+)\\(([^)]*)\\)".r
-    svg.xml.child map {
-      case g @ Elem(_,"g",attr,_,_*) => {
-        val transform = transformRegex.findAllMatchIn(
-          (attr.get("transform") getOrElse "").toString
-        ).map( m => (
-          m.group(1) -> m.group(2).split("[,\\s]").map(x => x.toDouble).toSeq)
-        ).toMap
-
-        def translate: ((Double, Double)) => (Double, Double) = (coords: (Double, Double)) => (coords._1 + transform("translate")(0), coords._2 + transform("translate")(1))
-        def scale: ((Double, Double)) => (Double, Double) = (coords: (Double, Double)) => (coords._1 * transform("scale")(0), coords._2 * transform("scale")(1))
-        val xforms = Map(
-          "translate" -> translate,
-          "scale" -> scale
-        )
-
-        g.child map {
-          case Elem(_,"path",attr,_,_*) => {
-            gc.beginPath()
-            val spaces = (attr.get("d") getOrElse "").toString.split("(?=[z])").mkString(" ")
-            doPath(gc, spaces, xforms, 0.0, 0.0)
-          }
-//          case t => println("Unknown tag2: " + t.label)
-          case _ =>
-        }
-      }
-//      case pcd @ PCData(text) => println(text)
-//      case t => println("Unknown tag: " + t.label)
-      case _ =>
-    }
-  }
-
-  val letterRegex = """[a-zA-Z]""".r
-
-  def doPath(gc: GraphicsContext, path: String, xform: Map[String,((Double, Double))=>(Double,Double)], cx: Double, cy: Double): Unit = {
-    path match {
-      case s"M${x} ${y} ${extra}" => {
-//        println(s"MOVE ${extra}")
-        val nc = xform("translate")(xform("scale") (x.toDouble, y.toDouble))
-        gc.moveTo(nc._1, nc._2)
-        extra(0) match {
-          case letterRegex(_*) => doPath(gc, extra, xform, nc._1, nc._2)
-          case _ => doPath(gc, "L"+extra, xform, nc._1, nc._2)
-        }
-      }
-      case s"m${x} ${y} ${extra}" => {
-//        println(s"MOVE ${extra}")
-        val nc = xform("scale") (x.toDouble, y.toDouble)
-        gc.moveTo(nc._1+cx, nc._2+cy)
-        extra(0) match {
-          case letterRegex(_*) => doPath(gc, extra, xform, nc._1+cx, nc._2+cy)
-          case _ => doPath(gc, "L"+extra, xform, nc._1+cx, nc._2+cy)
-        }
-      }
-      case s"c${x1} ${y1} ${x2} ${y2} ${dx} ${dy} ${extra}" => {
-//        println(s"cubic ${extra}")
-        val one = xform("scale") (x1.toDouble, y1.toDouble)
-        val two = xform("scale") (x2.toDouble, y2.toDouble)
-        val del = xform("scale") (dx.toDouble, dy.toDouble)
-        gc.bezierCurveTo(one._1+cx, one._2+cy, two._1+cx, two._2+cy, del._1+cx, del._2+cy)
-        extra(0) match {
-          case letterRegex() => doPath(gc, extra, xform, del._1+cx, del._2+cy)
-          case _ => doPath(gc, "c"+extra, xform, del._1+cx, del._2+cy)
-        }
-      }
-      case s"l${x1} ${y1} ${extra}" => {
-//        println(s"line ${extra}")
-        val nc = xform("scale") (x1.toDouble, y1.toDouble)
-        gc.lineTo(nc._1+cx, nc._2+cy)
-        extra(0) match {
-          case letterRegex() => doPath(gc, extra, xform, nc._1+cx, nc._2+cy)
-          case _ => doPath(gc, "l"+extra, xform, nc._1+cx, nc._2+cy)
-        }
-      }
-      case s"L${x1} ${y1} ${extra}" => {
-//        println(s"LINE ${extra}")
-        val nc = xform("translate")(xform("scale") (x1.toDouble, y1.toDouble))
-        gc.lineTo(nc._1, nc._2)
-        extra(0) match {
-          case letterRegex() => doPath(gc, extra, xform, nc._1, nc._2)
-          case _ => doPath(gc, "L"+extra, xform, nc._1, nc._2)
-        }
-      }
-      case s"z ${extra}" => {
-//        println("FIN")
-//        gc.fill ()
-        gc.stroke ()
-        gc.closePath()
-        doPath(gc, extra, xform, cx, cy)
-      }
-      case "z" => {
-//        println("FIN")
-        gc.fill()
-        gc.stroke()
-        gc.closePath()
-      }
-    }
+    canvas.children = svg.paths
   }
 
   def createMenus = new MenuBar {
